bd382678fafd91564a853e6830c47a3c5dcafac0
hank <hangryan@gmail.com>
2023-06-27 14:38:31 -0400

    06-27

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -14,7 +14,7 @@ const Home = () => {
                     </div>
                     <div className="col-lg-8 align-self-baseline">
                         <p className="text-white-75 mb-5">freelancer providing services for programming and web contents</p>
-                        <Link className="btn btn-primary btn-xl" to="/services">Join me and let's get cracking</Link>
+                        <Link className="btn btn-primary btn-xl" to="/about">Join me and let's get cracking</Link>
                     </div>
                 </div>
             </div>
